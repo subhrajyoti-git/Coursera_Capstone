fb7e77d398dfcd5e8828a707270fe9cd3b4822ae
Subhrajyoti Roy Chowdhury <subhrajyoti.roy.chowdhury@ericsson.com>
2019-07-03 14:13:33 -0400

    This notebook will be used for the Applied Data Science Capstone project

---
+++ Applied_Data_Science_Capstone.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# # This notebook will be used for the Applied Data Science Capstone project 
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
